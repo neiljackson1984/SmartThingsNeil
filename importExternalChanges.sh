2c908a7e700483728b65a423910f8ea1605c2cda
Neil Jackson <neil@rattnow.com>
2018-04-24 12:51:17 -0700

    checkpoint

--- importExternalChanges.sh
+++ importExternalChanges.sh
@@ -4,6 +4,7 @@
 nameOfTag=$(date +%Y-%m-%d-%H%M%S--external-changes)
 tagMessage="external changes available from all braids."
 testing=true
+nameOfBranchToWhichToImportChanges=master
 
 if $testing=="true"; then
 	git clone https://github.com/neiljackson1984/neil-smartThings
@@ -14,7 +15,7 @@ fi
 # we assume that we are starting out in the root of my smartThings git repository with some arbitrary branch 
 # checked out (probably the special branch that I will use to trigger Travis.)
 # we also assume that braid is installed.
-git checkout $(git rev-parse master)
+git checkout $(git rev-parse $nameOfBranchToWhichToImportChanges) # this puts us into a detached head state, which ensures that we will not muck up the master branch
 braid update
 git tag --annotate --message="$tagMessage" $nameOfTag
 git push --tags
@@ -22,5 +23,8 @@ git push --tags
 if $testing=="true"; then
 	popd
 	rm -rf neil-smartThings
+	git checkout -f $nameOfBranchToWhichToImportChanges;
 fi
+
+#we have just created a commit that is a child of the commit that $nameOfBranchToWhichToImportChanges currently points to.  We have tagged this commit with a uniquely-named tag and pushed the tag to github.
 
