af7175fd0301e97207f74bb0b35085eae72cdc96
Neil Jackson <neil@rattnow.com>
2018-04-24 13:35:51 -0700

    checkpoint

--- import-external-changes.sh
+++ import-external-changes.sh
@@ -16,7 +16,7 @@ fi
 # checked out (probably the special branch that I will use to trigger Travis.)
 # we also assume that braid is installed.
 git checkout $(git rev-parse $nameOfBranchToWhichToImportChanges) # this puts us into a detached head state, which ensures that we will not muck up the master branch
-braid update
+bundle exec braid update
 git tag --annotate --message="$tagMessage" $nameOfTag
 git push --tags
 
