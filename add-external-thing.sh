cd128f210ea8c1f73eeff1728f936af002bcd6e8
Neil Jackson <neil@rattnow.com>
2018-05-20 14:51:05 -0700

    added a convenience wrapper around braid add for adding the subdirectory braids that I end up needing to add frequently.

---
+++ add-external-thing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# takes two arguments, the first is the URL of a git repository.  The second is a path within that repository.
+# for example:
+#	add-external-thing "https://github.com/SmartThingsCommunity/SmartThingsPublic" "devicetypes/smartthings/virtual-switch.src"
+# invokes braid add to add a braid of the specified path within the specified repository, located at the same path within the current repository.  From the example arguments above, we will run:
+#  braid add \
+#   https://github.com/SmartThingsCommunity/SmartThingsPublic \
+#   --path=devicetypes/smartthings/virtual-switch.src \
+#   devicetypes/smartthings/virtual-switch.src
+
+braid add "$1" --path="$2" $2
