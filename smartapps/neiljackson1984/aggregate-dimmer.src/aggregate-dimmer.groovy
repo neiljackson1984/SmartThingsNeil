2b5564004aeb5659b81708be0ee0f7441438e1bc
Neil Jackson <neiljackson1984@gmail.com>
2018-05-23 22:31:51 -0700

    checkpoint

--- smartapps/neiljackson1984/aggregate-dimmer.src/aggregate-dimmer.groovy
+++ smartapps/neiljackson1984/aggregate-dimmer.src/aggregate-dimmer.groovy
@@ -15,7 +15,7 @@ definition(
 		+ "non-dimmable lights.",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
 )
 
 
