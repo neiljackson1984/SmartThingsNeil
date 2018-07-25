99e5d911abb6c5be5adae40512f89f65e1576cec
Travis CI User <travis@example.org>
2018-07-25 08:17:37 +0000

    external changes available as of 2018-07-25-081725
    numberOfChangedFiles: 2
    braidsFileChanged: 1
    
    changed files:
    .braids.json
    devicetypes/erocm123/inovelli-2-channel-smart-plug-nzw37-w-scene.src/inovelli-2-channel-smart-plug-nzw37-w-scene.groovy
    devicetypes/smartthings/virtual-dimmer-switch.src/virtual-dimmer-switch.groovy

--- devicetypes/smartthings/virtual-dimmer-switch.src/virtual-dimmer-switch.groovy
+++ devicetypes/smartthings/virtual-dimmer-switch.src/virtual-dimmer-switch.groovy
@@ -14,7 +14,11 @@
  *
  */
 metadata {
+<<<<<<< HEAD
     definition (name: "Virtual Dimmer Switch", namespace: "smartthings", author: "SmartThings", runLocally: /*true*/ /*this has to be false in order for SmartThings not to throw an error.*/ false, minHubCoreVersion: '000.021.00001', executeCommandsLocally: false /*true*/) {
+=======
+    definition (name: "Virtual Dimmer Switch", namespace: "smartthings", author: "SmartThings", runLocally: true, minHubCoreVersion: '000.021.00001', executeCommandsLocally: true, mnmn: "SmartThings", vid: "generic-dimmer") {
+>>>>>>> 7827a2449fbc67d57b9dfc8786686f52e6d8e716
         capability "Actuator"
         capability "Sensor"
         capability "Switch"
