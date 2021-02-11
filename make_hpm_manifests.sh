63dd29b84b9b998a557472e1c36a94da094fc06c
Neil Jackson <neil@rattnow.com>
2021-02-10 16:57:41 -0800

    created initial package manifest

---
+++ make_hpm_manifests.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+
+pathOfHpmExecutable=./hpm.exe
+pathOfRepositoryFile=hubitat_package_manager_data/repository.json
+pathOfpackageManifestFile=drivers/stelpro/stelpro-ki-zigbee-thermostat.src/package_manifest.json
+
+$pathOfHpmExecutable manifest-create     "$pathOfpackageManifestFile" --name="StelPro Ki Zigbee Thermostat" --author="Neil Jackson" --version="1.0" --heversion="2.1.9" --datereleased="2021-02-10"
+$pathOfHpmExecutable manifest-add-driver "$pathOfpackageManifestFile" --location="https://raw.githubusercontent.com/neiljackson1984/SmartThingsNeil/c89e585b14d3c49a4a8665b4c3326049977f8d97/drivers/stelpro/stelpro-ki-zigbee-thermostat.src/stelpro-ki-zigbee-thermostat.groovy" --required=true
+
+$pathOfHpmExecutable repository-create "$pathOfRepositoryFile" --author="Neil Jackson"  --githuburl="https://github.com/neiljackson1984/SmartThingsNeil"
+$pathOfHpmExecutable repository-add-package "$pathOfRepositoryFile" \
+    --manifest "https://raw.githubusercontent.com/neiljackson1984/SmartThingsNeil/master/drivers/stelpro/stelpro-ki-zigbee-thermostat.src/package_manifest.json" \
+    --category="Control" \
+    --description="stelpro ki zigbee thermostat driver" \
+    --tags "Climate Control","Temperature & Humidity","Zigbee"
